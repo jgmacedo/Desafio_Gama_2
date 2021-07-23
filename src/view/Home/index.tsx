7df2f5ad6844e40a20f5c4bbd6e4b03cc2baa37a
João Gabriel Abreu <jgabreumacedo@gmail.com>
2021-07-22 23:24:01 -0300

    Finalizado tutorial do youtube.

--- src/view/Home/index.tsx
+++ src/view/Home/index.tsx
@@ -23,15 +23,21 @@ const Home: React.FC = () => {
     )
   }, [])
 
+  const handleCart = ( index: number ) =>{
+    const productStore = JSON.stringify(data[index])
+    localStorage.setItem(`@Produto-${index}`, productStore)
+  }
+
   return (
     <Container>
       <section>
-        { data.map (prod =>(        
+        { data.map ((prod, index) =>(        
           <div className = "product-content" key = {prod.id}>
             <img src={prod.photo} alt="jojo part 1" width="200px" height="auto"/>
             <h4>{prod.name}</h4>
             <span>{prod.description}</span>
             <h6>{prod.price}</h6>
+            <button onClick={ ()=> handleCart(index)} type="submit" >Adicionar ao carrinho</button>
           </div>
         ))}
       </section>
--- src/view/Home/style.ts
+++ src/view/Home/style.ts
@@ -9,14 +9,16 @@ export const Container = styled.div`
         width: 100%;
         background: #e2e2e2;
         display: flex;
+        padding: 10px;
 
         .product-content{
             display: grid;
             text-align: center;
-            height: 350px;
+            height: 390px;
             background: #fff;
             border-radius: 12px;
             padding: 21px;
+            margin: 5px;
          }
     }
 `
