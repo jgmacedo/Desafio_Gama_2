884bc53ffb89e1af2da0728ecf43eebd9b1957d1
jgmacedo <83835320+jgmacedo@users.noreply.github.com>
2021-07-24 18:20:12 -0300

    Tentativa de utilizar a api falsa

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8888/produtos'
+    baseURL: 'https://github.com/jgmacedo/Archives/blob/main/produtos.json'
 })
 
 export default api;
