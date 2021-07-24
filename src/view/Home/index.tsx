e967f3d116c2ddabbf7a7d867b6f35b03ca52420
João Gabriel Abreu <jgabreumacedo@gmail.com>
2021-07-24 18:03:36 -0300

    Terminado o tutorial do professor.

--- src/view/Home/index.tsx
+++ src/view/Home/index.tsx
@@ -14,7 +14,9 @@ interface IProduct{
 
 const Home: React.FC = () => {
   
-  const [ data , setData ] = useState<IProduct[]>([]);
+  const [ data, setData ] = useState<IProduct[]>([]);
+  const [ cart, setCart ] = useState<IProduct[]>([]);
+
   useEffect(() =>{
     api.get('').then(
       response => {
@@ -23,13 +25,26 @@ const Home: React.FC = () => {
     )
   }, [])
 
-  const handleCart = ( index: number ) =>{
-    const productStore = JSON.stringify(data[index])
-    localStorage.setItem(`@Produto-${index}`, productStore)
+  const handleCart = (index: number) => {
+    let push: any = [...cart, cart.push(data[index])];
+    setCart(push);
+    const productStore = JSON.stringify(cart)
+    localStorage.setItem("@cart", productStore)    
   }
 
   return (
     <Container>
+      <div className="nav">
+        <div className="logo">
+          <img src="https://image.flaticon.com/icons/png/512/4277/4277125.png" alt="logo" width="140px" height="auto" />
+          <h1>Best Books</h1>
+        </div>
+        <div>
+          <img className="cart" src="https://image.flaticon.com/icons/png/512/709/709640.png" alt="cart" width="35px" height="auto"></img>
+          <span>{cart.length} - Itens </span>
+        </div>
+      </div>
+
       <section>
         { data.map ((prod, index) =>(        
           <div className = "product-content" key = {prod.id}>
--- src/view/Home/style.ts
+++ src/view/Home/style.ts
@@ -4,12 +4,34 @@ export const Container = styled.div`
     width: 80%;
     margin: auto;
 
+    .logo {
+        display:flex;
+        align-items: center;
+        justify-content:space-between;
+        font-size: 3.4em;
+        margin-top: 10px;
+    }
+
+    .nav{
+        display:flex;
+        align-items: center;
+        justify-content:space-between;
+
+        .cart{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+    }
+    
     section{
         height: 100vh;
         width: 100%;
         background: #e2e2e2;
         display: flex;
         padding: 10px;
+        justify-content: space-around;
+
 
         .product-content{
             display: grid;
@@ -19,6 +41,7 @@ export const Container = styled.div`
             border-radius: 12px;
             padding: 21px;
             margin: 5px;
+            max-width: 80%;
          }
     }
 `
